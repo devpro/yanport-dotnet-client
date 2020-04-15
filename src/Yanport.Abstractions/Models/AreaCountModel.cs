6756cdd4004f0ad83e1f8674b8e9e0259171d990
Bertrand THOMAS <bertrand@devpro.fr>
2020-04-15 12:29:24 +0200

    Add 3 properties to AreaCountModel (#4)
    Dump version to 1.1.3

--- src/Yanport.Abstractions/Models/AreaCountModel.cs
+++ src/Yanport.Abstractions/Models/AreaCountModel.cs
@@ -4,6 +4,15 @@ namespace Devpro.Yanport.Abstractions.Models
 {
     public class AreaCountModel
     {
+        [JsonProperty("BEDROOM")]
+        public int Bedroom { get; set; }
+
+        [JsonProperty("GARDEN")]
+        public int Garden { get; set; }
+
+        [JsonProperty("PARKING")]
+        public int Parking { get; set; }
+
         [JsonProperty("TERRACE")]
         public int Terrace { get; set; }
     }
